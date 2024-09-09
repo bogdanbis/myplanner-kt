26bc95cc2ce0cd62f86dff1eb48ce8a4d0244931
Bogdan Negoita <negoitabogdan@icloud.com>
2024-09-09 13:16:32 +0300

    Dev CORS policy.

--- MyPlannerKT/src/main/kotlin/ro/bogdannegoita/myplannerkt/security/SecurityConfiguration.kt
+++ MyPlannerKT/src/main/kotlin/ro/bogdannegoita/myplannerkt/security/SecurityConfiguration.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.DispatcherType.ERROR
 import jakarta.servlet.DispatcherType.FORWARD
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.AuthenticationManager
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -12,6 +13,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import ro.bogdannegoita.myplannerkt.security.jwt.JwtFilter
 
 @Configuration
@@ -19,7 +22,29 @@ import ro.bogdannegoita.myplannerkt.security.jwt.JwtFilter
 class SecurityConfiguration(val jwtTokenFilter: JwtFilter) {
 
     @Bean
+    @Profile("dev")
+    fun securityFilterChainDev(http: HttpSecurity): SecurityFilterChain {
+        applyHttpSecurityRules(http)
+        http
+            .cors { httpSecurityCorsConfigurer ->
+                val configuration = CorsConfiguration()
+                configuration.allowedOrigins = listOf("*")
+                configuration.allowedMethods = listOf("*")
+                configuration.allowedHeaders = listOf("*")
+                val source = UrlBasedCorsConfigurationSource()
+                source.registerCorsConfiguration("/api/**", configuration)
+                httpSecurityCorsConfigurer.configurationSource(source)
+            }
+        return http.build()
+    }
+
+    @Bean
     fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        applyHttpSecurityRules(http)
+        return http.build()
+    }
+
+    private fun applyHttpSecurityRules(http: HttpSecurity): HttpSecurity {
         http
             .csrf { it.disable() }
             .exceptionHandling {
@@ -39,7 +64,7 @@ class SecurityConfiguration(val jwtTokenFilter: JwtFilter) {
                     .anyRequest().authenticated()
             }
             .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter::class.java)
-        return http.build()
+        return http
     }
 
     @Bean
