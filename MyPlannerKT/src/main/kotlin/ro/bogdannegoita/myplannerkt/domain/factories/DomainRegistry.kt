dc6c73a91ba48026fc5a0fa46d7ca13359256471
Bogdan Negoita <negoitabogdan@icloud.com>
2024-11-22 18:57:33 +0200

    Fix: Changes did not sync across sessions.

--- MyPlannerKT/src/main/kotlin/ro/bogdannegoita/myplannerkt/domain/factories/DomainRegistry.kt
+++ MyPlannerKT/src/main/kotlin/ro/bogdannegoita/myplannerkt/domain/factories/DomainRegistry.kt
@@ -1,13 +1,11 @@
 package ro.bogdannegoita.myplannerkt.domain.factories
 
-import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
 import ro.bogdannegoita.myplannerkt.domain.Plan
 import ro.bogdannegoita.myplannerkt.domain.PlanProgress
 import java.util.*
 
 @Component
-@Scope("prototype")
 class DomainRegistry {
     val plans = myPlannerCache<UUID, Plan>()
     val planProgress = myPlannerCache<UUID, PlanProgress>()
