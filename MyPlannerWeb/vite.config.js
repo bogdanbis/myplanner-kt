dc49a4e097d8cd2934e3152529e38000058a6da6
Bogdan Negoita <negoitabogdan@icloud.com>
2025-07-02 13:10:15 +0300

    Web: Update dependencies.

--- MyPlannerWeb/vite.config.js
+++ MyPlannerWeb/vite.config.js
@@ -1,22 +1,28 @@
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [
-		vue({
-			template: {
-				compilerOptions: {
-					isCustomElement: (tag) => tag === 'circle-progress',
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd())
+	return {
+		plugins: [
+			vue({
+				template: {
+					compilerOptions: {
+						isCustomElement: (tag) => tag === 'circle-progress',
+					},
 				},
+			}),
+		],
+		resolve: {
+			alias: {
+				'@': fileURLToPath(new URL('./src', import.meta.url)),
 			},
-		}),
-	],
-	resolve: {
-		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url)),
 		},
-	},
+		build: {
+			sourcemap: false,
+			outDir: env.VITE_OUT_DIR,
+		},
+	}
 })
