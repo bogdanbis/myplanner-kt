35ac1415e8c7f83a0adf5cac090fcac2d499664a
Bogdan Negoita <negoitabogdan@icloud.com>
2025-07-06 13:09:32 +0300

    Fix: Page state not updated after logging out.

--- MyPlannerWeb/src/components/UserInfoButton.vue
+++ MyPlannerWeb/src/components/UserInfoButton.vue
@@ -12,17 +12,15 @@
 <script setup>
 import { useAuthStore } from '@/store/auth.js';
 import { computed } from 'vue';
-import { useRouter } from 'vue-router';
 import { useToast } from 'vue-toastification';
 
 const user = computed(() => useAuthStore().user);
 
 const authStore = useAuthStore();
 const toast = useToast();
-const router = useRouter();
 const logOut = () => {
 	authStore.reset();
 	toast.info('You have been logged out.');
-	router.push('/');
+	window.location = '/';
 }
 </script>
